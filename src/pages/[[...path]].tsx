8fb10ce4adaa6077695a1b3dd789bd368da4141e
ashandw <ashan.wijayawardhane@akqa.com>
2023-12-21 11:00:17 +1300

    Update [[...path]].tsx

--- src/pages/[[...path]].tsx
+++ src/pages/[[...path]].tsx
@@ -14,7 +14,7 @@ import { SitecorePageProps } from 'lib/page-props';
 import { sitecorePagePropsFactory } from 'lib/page-props-factory';
 import { componentBuilder } from 'temp/componentBuilder';
 //import { sitemapFetcher } from 'lib/sitemap-fetcher';
-
+export const runtime = 'experimental-edge';
 const SitecorePage = ({
   notFound,
   componentProps,
