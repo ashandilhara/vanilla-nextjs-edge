2ed294dfacda692f007cc88dc69149d61bc48e53
ashandw <ashan.wijayawardhane@akqa.com>
2023-12-21 09:37:55 +1300

    Update [[...path]].tsx

--- src/pages/[[...path]].tsx
+++ src/pages/[[...path]].tsx
@@ -1,74 +1,63 @@
-//import { useEffect } from 'react';
-//import { GetStaticPaths, GetStaticProps } from 'next';
+import { useEffect } from 'react';
+import { GetStaticPaths, } from 'next';
 import NotFound from 'src/NotFound';
 import Layout from 'src/Layout';
-//import {
-//RenderingType,
-//SitecoreContext,
-//ComponentPropsContext,
-//EditingComponentPlaceholder,
-//StaticPath,
-//} from '@sitecore-jss/sitecore-jss-nextjs';
-//import { handleEditorFastRefresh } from '@sitecore-jss/sitecore-jss-nextjs/utils';
+import {
+  RenderingType,
+  SitecoreContext,
+  ComponentPropsContext,
+  EditingComponentPlaceholder,
+  StaticPath,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+import { handleEditorFastRefresh } from '@sitecore-jss/sitecore-jss-nextjs/utils';
 import { SitecorePageProps } from 'lib/page-props';
 //import { sitecorePagePropsFactory } from 'lib/page-props-factory';
-//import { componentBuilder } from 'temp/componentBuilder';
-//import { sitemapFetcher } from 'lib/sitemap-fetcher';
-
-export const runtime = 'experimental-edge';
+import { componentBuilder } from 'temp/componentBuilder';
+import { sitemapFetcher } from 'lib/sitemap-fetcher';
 
 const SitecorePage = ({
   notFound,
-  //componentProps,
+  componentProps,
   layoutData,
   headLinks,
 }: SitecorePageProps): JSX.Element => {
-  {
-    /*useEffect(() => {
+  useEffect(() => {
     // Since Sitecore editors do not support Fast Refresh, need to refresh editor chromes after Fast Refresh finished
     handleEditorFastRefresh();
-  }, []);*/
-  }
+  }, []);
 
   if (notFound || !layoutData.sitecore.route) {
-    // Shouldn't hit this (as long as 'notFound' is being returned below), but just to be safe111
+    // Shouldn't hit this (as long as 'notFound' is being returned below), but just to be safe
     return <NotFound />;
   }
 
-  //const isEditing = layoutData.sitecore.context.pageEditing;
-  //const isComponentRendering =
-  //layoutData.sitecore.context.renderingType === RenderingType.Component;
+  const isEditing = layoutData.sitecore.context.pageEditing;
+  const isComponentRendering =
+    layoutData.sitecore.context.renderingType === RenderingType.Component;
 
-  //return (
-  //<ComponentPropsContext value={componentProps}>
-  //<SitecoreContext
-  //componentFactory={componentBuilder.getComponentFactory({ isEditing })}
-  //layoutData={layoutData}
-  //>
-  {
-    /*
+  return (
+    <ComponentPropsContext value={componentProps}>
+      <SitecoreContext
+        componentFactory={componentBuilder.getComponentFactory({ isEditing })}
+        layoutData={layoutData}
+      >
+        {/*
           Sitecore Pages supports component rendering to avoid refreshing the entire page during component editing.
           If you are using Experience Editor only, this logic can be removed, Layout can be left.
-        
+        */}
         {isComponentRendering ? (
           <EditingComponentPlaceholder rendering={layoutData.sitecore.route} />
         ) : (
           <Layout layoutData={layoutData} headLinks={headLinks} />
-        )}*/
-  }
-  //<Layout layoutData={layoutData} headLinks={headLinks} />
-  //</SitecoreContext>
-  //</ComponentPropsContext>
-  //);
-
-  return <Layout layoutData={layoutData} headLinks={headLinks} />;
+        )}
+      </SitecoreContext>
+    </ComponentPropsContext>
+  );
 };
 
 // This function gets called at build and export time to determine
 // pages for SSG ("paths", as tokenized array).
-{
-  /*export const getStaticPaths: GetStaticPaths = async (context) => {
-  console.log('Ashan2 - in');
+export const getStaticPaths: GetStaticPaths = async (context) => {
   // Fallback, along with revalidate in getStaticProps (below),
   // enables Incremental Static Regeneration. This allows us to
   // leave certain (or all) paths empty if desired and static pages
@@ -92,31 +81,26 @@ const SitecorePage = ({
     fallback = process.env.EXPORT_MODE ? false : fallback;
   }
 
-  console.log('Ashan2: ' + paths.length);
   return {
     paths,
     fallback,
   };
-};*/
-}
+};
 
 // This function gets called at build time on server-side.
 // It may be called again, on a serverless function, if
 // revalidation (or fallback) is enabled and a new request comes in.
-{
-  /*export const getStaticProps: GetStaticProps = async (context) => {
-  console.log('Ashan1 - in');
-  const props = await sitecorePagePropsFactory.create(context);
-  console.log('Ashan1: ' + props);
-  return {
-    props,
+//export const getStaticProps: GetStaticProps = async (context) => {
+  //const props = await sitecorePagePropsFactory.create(context);
+
+  //return {
+    ////props,
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
     // - At most once every 5 seconds
     revalidate: 5, // In seconds
-    notFound: props.notFound, // Returns custom 404 page with a status code of 404 when true
-  };
-};*/
-}
+    //notFound: props.notFound, // Returns custom 404 page with a status code of 404 when true
+  //};
+//};
 
 export default SitecorePage;
