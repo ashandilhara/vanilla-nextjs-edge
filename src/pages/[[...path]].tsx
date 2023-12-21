3ec73041068fbedb4865bb19a2458cfb7c2d3d75
ashandw <ashan.wijayawardhane@akqa.com>
2023-12-21 14:10:50 +1300

    Update [[...path]].tsx

--- src/pages/[[...path]].tsx
+++ src/pages/[[...path]].tsx
@@ -1,26 +1,25 @@
 import { useEffect } from 'react';
 import { GetServerSideProps } from 'next';
 import NotFound from 'src/NotFound';
-import Layout from 'src/Layout';
-import {
-  RenderingType,
-  SitecoreContext,
-  ComponentPropsContext,
-  EditingComponentPlaceholder,
-  //StaticPath,
-} from '@sitecore-jss/sitecore-jss-nextjs';
+//import Layout from 'src/Layout';
+import //RenderingType,
+//SitecoreContext,
+//ComponentPropsContext,
+//EditingComponentPlaceholder,
+//StaticPath,
+'@sitecore-jss/sitecore-jss-nextjs';
 import { handleEditorFastRefresh } from '@sitecore-jss/sitecore-jss-nextjs/utils';
 import { SitecorePageProps } from 'lib/page-props';
 import { sitecorePagePropsFactory } from 'lib/page-props-factory';
-import { componentBuilder } from 'temp/componentBuilder';
+//import { componentBuilder } from 'temp/componentBuilder';
 //import { sitemapFetcher } from 'lib/sitemap-fetcher';
 export const runtime = 'experimental-edge';
 const SitecorePage = ({
   notFound,
-  componentProps,
+  //componentProps,
   layoutData,
-  headLinks,
-}: SitecorePageProps): JSX.Element => {
+}: //headLinks,
+SitecorePageProps): JSX.Element => {
   useEffect(() => {
     // Since Sitecore editors do not support Fast Refresh, need to refresh editor chromes after Fast Refresh finished
     handleEditorFastRefresh();
@@ -31,28 +30,11 @@ const SitecorePage = ({
     return <NotFound />;
   }
 
-  const isEditing = layoutData.sitecore.context.pageEditing;
-  const isComponentRendering =
-    layoutData.sitecore.context.renderingType === RenderingType.Component;
+  //const isEditing = layoutData.sitecore.context.pageEditing;
+  //const isComponentRendering =
+  //layoutData.sitecore.context.renderingType === RenderingType.Component;
 
-  return (
-    <ComponentPropsContext value={componentProps}>
-      <SitecoreContext
-        componentFactory={componentBuilder.getComponentFactory({ isEditing })}
-        layoutData={layoutData}
-      >
-        {/*
-          Sitecore Pages supports component rendering to avoid refreshing the entire page during component editing.
-          If you are using Experience Editor only, this logic can be removed, Layout can be left.
-        */}
-        {isComponentRendering ? (
-          <EditingComponentPlaceholder rendering={layoutData.sitecore.route} />
-        ) : (
-          <Layout layoutData={layoutData} headLinks={headLinks} />
-        )}
-      </SitecoreContext>
-    </ComponentPropsContext>
-  );
+  return <div>Hello World</div>;
 };
 
 // This function gets called at build and export time to determine
