b287b95e68b4454ff60ff21f642f517b551cc608
ashandw <ashan.wijayawardhane@akqa.com>
2023-12-21 16:23:50 +1300

    Update [[...path]].tsx

--- src/pages/[[...path]].tsx
+++ src/pages/[[...path]].tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+//import { useEffect } from 'react';
 import { GetServerSideProps } from 'next';
 //import NotFound from 'src/NotFound';
 import Layout from 'src/Layout';
@@ -9,7 +9,7 @@ import {
   EditingComponentPlaceholder,
   //StaticPath,
 } from '@sitecore-jss/sitecore-jss-nextjs';
-import { handleEditorFastRefresh } from '@sitecore-jss/sitecore-jss-nextjs/utils';
+//import { handleEditorFastRefresh } from '@sitecore-jss/sitecore-jss-nextjs/utils';
 import { SitecorePageProps } from 'lib/page-props';
 //import { sitecorePagePropsFactory } from 'lib/page-props-factory';
 import { componentBuilder } from 'temp/componentBuilder';
