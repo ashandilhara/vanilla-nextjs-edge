74d3b67c649ac6f880ac98bc39cc69849a394f06
ashandw <ashan.wijayawardhane@akqa.com>
2023-12-21 15:48:01 +1300

    Revert changes

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -11,6 +11,7 @@ const NotFound = (): JSX.Element => (
     <div style={{ padding: 10 }}>
       <h1>Page not found</h1>
       <p>This page does not exist.</p>
+      <a href="/">Go to the Home page</a>
     </div>
   </>
 );
---
+++ src/pages/[[...path]].tsx
@@ -0,0 +1,79 @@
+import { useEffect } from 'react';
+import { GetServerSideProps } from 'next';
+//import NotFound from 'src/NotFound';
+import Layout from 'src/Layout';
+import {
+  RenderingType,
+  SitecoreContext,
+  ComponentPropsContext,
+  EditingComponentPlaceholder,
+  //StaticPath,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+import { handleEditorFastRefresh } from '@sitecore-jss/sitecore-jss-nextjs/utils';
+import { SitecorePageProps } from 'lib/page-props';
+//import { sitecorePagePropsFactory } from 'lib/page-props-factory';
+import { componentBuilder } from 'temp/componentBuilder';
+//import { sitemapFetcher } from 'lib/sitemap-fetcher';
+export const runtime = 'experimental-edge';
+const SitecorePage = ({
+  notFound,
+  componentProps,
+  layoutData,
+  headLinks,
+}: SitecorePageProps): JSX.Element => {
+  useEffect(() => {
+    // Since Sitecore editors do not support Fast Refresh, need to refresh editor chromes after Fast Refresh finished
+    handleEditorFastRefresh();
+  }, []);
+
+  if (notFound || !layoutData.sitecore.route) {
+    // Shouldn't hit this (as long as 'notFound' is being returned below), but just to be safe
+    //return <NotFound />;
+
+    return <div>Ashan Hello World!!!</div>;
+  }
+
+  const isEditing = layoutData.sitecore.context.pageEditing;
+  const isComponentRendering =
+    layoutData.sitecore.context.renderingType === RenderingType.Component;
+
+  return (
+    <ComponentPropsContext value={componentProps}>
+      <SitecoreContext
+        componentFactory={componentBuilder.getComponentFactory({ isEditing })}
+        layoutData={layoutData}
+      >
+        {/*
+          Sitecore Pages supports component rendering to avoid refreshing the entire page during component editing.
+          If you are using Experience Editor only, this logic can be removed, Layout can be left.
+        */}
+        {isComponentRendering ? (
+          <EditingComponentPlaceholder rendering={layoutData.sitecore.route} />
+        ) : (
+          <Layout layoutData={layoutData} headLinks={headLinks} />
+        )}
+      </SitecoreContext>
+    </ComponentPropsContext>
+  );
+};
+
+// This function gets called at build and export time to determine
+// pages for SSG ("paths", as tokenized array).
+
+// This function gets called at build time on server-side.
+// It may be called again, on a serverless function, if
+// revalidation (or fallback) is enabled and a new request comes in.
+export const getServerSideProps: GetServerSideProps = async () => {
+  //const props = await sitecorePagePropsFactory.create(context);
+  const props = {} as SitecorePageProps;
+  return {
+    props,
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 5 seconds
+    //revalidate: 5, // In seconds
+    notFound: props.notFound, // Returns custom 404 page with a status code of 404 when true
+  };
+};
+
+export default SitecorePage;
