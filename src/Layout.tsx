36cddb0a48caaf9833c3f8de16364e2d9192a228
ashandw <ashan.wijayawardhane@akqa.com>
2024-01-09 12:39:32 +1300

    Change Placeholder key

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -39,7 +39,9 @@ const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
 
       <Navigation />
       {/* root placeholder for the app, which we add components to using route data */}
-      <div className="container">{route && <Placeholder name="jss-main" rendering={route} />}</div>
+      <div className="container">
+        {route && <Placeholder name="headless-main" rendering={route} />}
+      </div>
     </>
   );
 };
